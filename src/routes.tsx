08dccaba0744917fe0e774974d06ad9d0f2b1122
Varun-Kelkar <vnkelkar11@gmail.com>
2025-06-03 14:16:33 +0530

    added base route & github pages related config

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,17 +3,21 @@ import App from "./App";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import Explore from "./pages/Explore/Explore";
 
-export const routes = createBrowserRouter([
+export const routes = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { index: true, Component: LandingPage },
+        {
+          path: "explore",
+          Component: Explore,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, Component: LandingPage },
-      {
-        path: "explore",
-        // Lazy load the Explore component
-        Component: Explore,
-      },
-    ],
-  },
-]);
+    basename: "/djikstras-algorithm-visualizer",
+  }
+);
